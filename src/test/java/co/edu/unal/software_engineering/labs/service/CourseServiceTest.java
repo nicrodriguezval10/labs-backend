85139e089afafad90e465f703a50e9adb55cbe6f
Nicolás Rodríguez <nicrodriguezval@unal.edu.co>
2020-06-09 16:16:30 -0500

    isRightCoursePOJOTest

--- src/test/java/co/edu/unal/software_engineering/labs/service/CourseServiceTest.java
+++ src/test/java/co/edu/unal/software_engineering/labs/service/CourseServiceTest.java
@@ -1,6 +1,5 @@
 package co.edu.unal.software_engineering.labs.service;
 
-import co.edu.unal.software_engineering.labs.model.Course;
 import co.edu.unal.software_engineering.labs.pojo.CoursePOJO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,30 +9,25 @@ import org.mockito.junit.MockitoJUnitRunner;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-@RunWith( MockitoJUnitRunner.class)
-public class CourseServiceTest{
+@RunWith(MockitoJUnitRunner.class)
+public class CourseServiceTest {
 
     @InjectMocks
     private CourseService courseService;
 
-
-    public CourseServiceTest( ){ }
-
-
-    @Test
-    public void mapperCourseEntityTest( ){
-        CoursePOJO pojo = new CoursePOJO( );
-        pojo.setCourseName( "Course Test # 1" );
-        pojo.setDurationHours( 400 );
-        Course course = courseService.mapperCourseEntity( pojo );
-
-        assertNotNull( course );
-        assertNotNull( course.getCourseName( ) );
-        assertNotNull( course.getDurationHours( ) );
-
-        assertEquals( course.getCourseName( ), pojo.getCourseName( ) );
-        assertEquals( course.getDurationHours( ), pojo.getDurationHours( ) );
+    public CourseServiceTest() {
 
     }
 
+    @Test
+    public void isRightCoursePOJOTest() {
+        CoursePOJO pojo = new CoursePOJO( );
+        pojo.setCourseName(" ");
+        pojo.setDurationHours(250);
+        Boolean course = courseService.isRightCoursePOJO(pojo);
+        Boolean expected = false;
+
+        assertNotNull(course);
+        assertEquals(expected, course);
+    }
 }
